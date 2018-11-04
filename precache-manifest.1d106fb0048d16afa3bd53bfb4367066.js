d0206799b266cf3bd6dbb8aff2bc319d26482fcf
Santhosh Bonala <S530859@nwmissouri.edu>
2018-11-03 20:49:46 -0500

    Updates

---
+++ precache-manifest.1d106fb0048d16afa3bd53bfb4367066.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "26eda6e7c462416b8370",
+    "url": "/testing-12/static/js/runtime~main.26eda6e7.js"
+  },
+  {
+    "revision": "3ca54f04ea45f2bd6172",
+    "url": "/testing-12/static/js/main.3ca54f04.chunk.js"
+  },
+  {
+    "revision": "547408c0d674b28f8119",
+    "url": "/testing-12/static/js/1.547408c0.chunk.js"
+  },
+  {
+    "revision": "3ca54f04ea45f2bd6172",
+    "url": "/testing-12/static/css/main.31de7e8e.chunk.css"
+  },
+  {
+    "revision": "547408c0d674b28f8119",
+    "url": "/testing-12/static/css/1.19ff6fb2.chunk.css"
+  },
+  {
+    "revision": "829b67ae21912cbb4ec86aaa43e1d641",
+    "url": "/testing-12/index.html"
+  }
+];
